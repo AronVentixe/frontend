311b0588cdf8c56dee090d3fcd98a9aa3a05d8fb
Aron Hektor <aronhektor@hotmail.com>
2025-06-08 18:33:57 +0200

    fixed routing issue when directlink got error 404

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { copyFileSync } from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
-  plugins: [react()],
+  base: './',
+  plugins: [
+    react(),
+
+    // eget plugin för att kopiera config efter build
+    {
+      name: 'copy-staticwebapp-config',
+      closeBundle() {
+        copyFileSync('staticwebapp.config.json', 'dist/staticwebapp.config.json')
+      }
+    }
+  ]
 })
